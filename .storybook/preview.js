c9168063e13ca0a1b3f2cf9edcaca34ae2749eb7
Don Isaac <donald.isaac@gmail.com>
2021-05-26 14:27:47 -0400

    styles cleanup, improvements to fraction and worksheet styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { addDecorator } from "@storybook/react"
 import { withTests } from "@storybook/addon-jest"
 
 import results from "./.jest-test-results.json"
+import "normalize.css/normalize.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,15 @@
 import React, { FC } from "react"
-import { BrowserRouter as Router, Link } from "react-router-dom"
+// import { BrowserRouter as Router, Link } from "react-router-dom"
 import logo from "./logo.svg"
 import "./App.scss"
-import { Fraction, Operation } from "../lib"
-import { Question } from "../lib"
-// Import { FillBlanksQuestion } from "./question/FillBlanksQuestion"
-import { QuestionGrid } from "./page/QuestionGrid"
 import { Worksheet } from "./page/Worksheet"
 
-const App = () => {
-    const testFractions = [
-        new Fraction(1, 2),
-        new Fraction(3, 7, true),
-        new Fraction(5, 15),
-        new Fraction(1, 3, true),
-    ]
-    const testFractionsWithParens = [
-        new Fraction(7, 8),
-        new Fraction(4, 5, true),
-    ]
-    const testQuestions: Question[] = [{ left: new Fraction(1, 2), right: new Fraction(1, 2), operation: Operation.Addition }]
+const App: FC = () => {
 
     return (
         <div className="App">
             <header className="App-header">
-                <Worksheet cols={6} rows={16} />
+                <Worksheet cols={5} rows={16} />
             </header>
         </div>
     )
