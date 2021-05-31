3aa2ab835fc3b7040f5aef8767ba06ad65a06452
Don Isaac <donald.isaac@gmail.com>
2021-05-31 14:05:11 -0400

    split linting/typechecking and building/testing into separate workflows

--- src/state/questions.ts
+++ src/state/questions.ts
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import { CaseReducerWithPrepare, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { Question, raise } from "../lib"
 
 
